d39590dee44091cbf01ddbfcc7b4f8d24b0e3623
Edward Ramos <ramosedward21@gmail.com>
2021-06-18 02:46:51 -0500

    FIX: Redireccionando layouts

--- client/src/components/TextFields/InputField.jsx
+++ client/src/components/TextFields/InputField.jsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const StyledInputField = withStyles({
-    root: {
-        borderRadius: '4px 4px 0px 0px',
-        width: '550px',
-    }
-})(TextField)
+  root: {
+    borderRadius: "4px 4px 0px 0px",
+    width: "550px",
+  },
+})(TextField);
 
 const InputField = (props) => {
-    return(
-        <StyledInputField 
-            variant="filled" 
-            label={props.label}/>
-    )
-}
+  return <StyledInputField variant="filled" label={props.label} />;
+};
 
-export default InputField
+export default InputField;
---
+++ client/src/layouts/NavBar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Image from "material-ui-image";
+import SearchField from "../components/TextFields/SearchField";
+import theme from "../themes/themes";
+
+const StyledNavBar = withStyles({
+  root: {
+    background: theme.palette.primary.main,
+    height: "14.5%",
+    lineHeight: "24px",
+    letterSpacing: "0.18px",
+    textTransform: "uppercase",
+  },
+})(AppBar);
+
+const StyledTypography = withStyles({
+  root: {
+    color: "#FFFFFF",
+    textDecoration: "none",
+    margin: "0 20px",
+  },
+})(Typography);
+
+const Logo = withStyles({
+  root: {
+    width: "96px",
+    margin: "0 20px",
+  },
+})(Typography);
+/*Cambiar por Image cuando esté listo el logo */
+
+const NavBar = () => {
+  return (
+    <StyledNavBar>
+      <Toolbar>
+        <Logo />
+        {/*Espacio vacío para el logo*/}
+        <Link href="#">
+          <StyledTypography variant="h5">Inicio</StyledTypography>
+        </Link>
+        <SearchField placeholder="Buscar Cliente o Servicio" />
+        <Link href="#">
+          <StyledTypography variant="h5">Registrarse</StyledTypography>
+        </Link>
+        <Link href="#">
+          <StyledTypography variant="h5">Iniciar Sesión</StyledTypography>
+        </Link>
+      </Toolbar>
+    </StyledNavBar>
+  );
+};
+
+export default NavBar;
