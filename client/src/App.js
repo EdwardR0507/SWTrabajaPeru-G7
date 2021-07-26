d06279987c247fd93eabe6c4b1ec9b7f07d8421c
Mishell <53318144+MishellGomezCavero@users.noreply.github.com>
2021-07-26 02:51:20 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import SignIn from "./pages/login";
 import SignUp from "./pages/register";
 import EditProfile from "./pages/editProfile"
 import ManageServices from "./pages/services";
+import socialProfile from "./pages/socialProfile";
 const GlobalStyles = withStyles({
   "@global": {
     "html, body": {
@@ -27,6 +28,7 @@ function App() {
           <Route exact path="/signup" component={SignUp} />
           <Route path="/editProfile" component={EditProfile}/>
           <Route path="/manageservices" component={ManageServices} />
+          <Route path="/socialprofile" component={socialProfile} />
           <Route path="*" component={() => <h1>404 NOT FOUND</h1>} />
         </Switch>
       </BrowserRouter>
