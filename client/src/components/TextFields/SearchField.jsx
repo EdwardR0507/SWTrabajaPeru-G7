3a8cbab8792fec1f16aeda877b0678a80c578c06
nayshasantiago <naysha.santiago@unmsm.edu.pe>
2021-07-29 02:09:29 -0500

    add :autocomplete

--- client/src/components/TextFields/SearchField.jsx
+++ client/src/components/TextFields/SearchField.jsx
@@ -1,58 +1,39 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import theme from '../../themes/themes';
-=======
-//Importacion de Librerias
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import theme from "../../themes/themes";
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
->>>>>>> Stashed changes
 
-//
+
 const StyledSearchField = withStyles({
-    root:{
-        width: '577px',
-        height: '48px',
-        background: '#FFFFFF',
-        boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
-        borderRadius: '28px',
-        color: theme.palette.secondary.main,
-        fontWeight: '500',
-        fontSize: '14px',
-        lineHeight: '16px',
-        letterSpacing: '1.25px',
-        textTransform: 'uppercase',
-        display: 'flex',
-        alignContent: 'flex-start',
-        margin: '20px auto',
-        padding: '0 10px'
-    }
+  root: {
+    width: "577px",
+    height: "48px",
+    background: theme.colorLetter.primary.main,
+    boxShadow: theme.boxShadowButton.primary.main,
+    borderRadius: "28px",
+    color: theme.palette.secondary.main,
+    fontWeight: "500",
+    fontSize: "14px",
+    lineHeight: "16px",
+    letterSpacing: "1.25px",
+    textTransform: "uppercase",
+    display: "flex",
+    alignContent: "flex-start",
+    margin: "20px auto",
+    padding: "0 10px",
+  },
 })(Paper);
 
-/*
 const StyledInputBase = withStyles({
-<<<<<<< Updated upstream
-    root: {
-        width: '100%'
-    }
-})(InputBase)
-=======
   root: {
     width: "100%",
   },
 })(InputBase);
-*/
->>>>>>> Stashed changes
 
 //Estilo de componente Autocomplete
 const StyledAutocomplete = withStyles({
@@ -63,28 +44,14 @@ const StyledAutocomplete = withStyles({
   },
 })(Autocomplete);
 
-
 const Prueba1 = [
   { Name: 'Gianela Malqui', Servicio: "Secretaria" },
   { Name: 'Gianela Malqui', Servicio: "Secretaria" },
   { Name: 'Gianela Malqui', Servicio: "Secretaria" },
 ];
 
-//
-const SearchField = (props) => {
-<<<<<<< Updated upstream
-    return(
-        <StyledSearchField>
-            <IconButton>
-                <SearchIcon />
-            </IconButton>
-            <StyledInputBase 
-                placeholder={props.placeholder} />
-        </StyledSearchField>
-    )
-};
 
-=======
+const SearchField = (props) => {
   return (
     <StyledSearchField>
       <IconButton>
@@ -94,7 +61,7 @@ const SearchField = (props) => {
 
         /*Mostrará info de service y user */
         options={Prueba1.map((option) => option.Name)}
-
+        
         renderInput={(params) => (
           <TextField {...params} width="577px"
           />
@@ -103,6 +70,5 @@ const SearchField = (props) => {
     </StyledSearchField>
   );
 };
-//Exportacion de elemento SearchField
->>>>>>> Stashed changes
+
 export default SearchField;
