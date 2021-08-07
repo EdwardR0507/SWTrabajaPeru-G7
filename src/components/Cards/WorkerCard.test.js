c6d5a5edb89fe40d72eeb3b0689796e271b4a264
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-08-06 23:00:50 -0500

    test: Pruebas unitarias de Cards, 40% de cobertura alcanzada

--- src/components/Cards/WorkerCard.test.js
+++ src/components/Cards/WorkerCard.test.js
@@ -10,11 +10,18 @@ describe("<WorkerCard></WorkerCard>", () => {
     expect(wrapper).toMatchSnapshot()
   });
   it('render WorkerCard with user', () => {
-    const user = {
+    const worker = {
       us_nombres: "Test Name",
       us_celular: "987654321",
     }
-    render(<WorkerCard user={user} />);
+    render(<WorkerCard worker={worker} />);
     expect(screen.getByText(/Test Name/i)).toBeInTheDocument()
+  })
+  it('render Calification 0', () => {
+    const worker = {
+      us_calificacion: 0.000
+    }
+    render(<WorkerCard worker={worker} />);
+    expect(screen.getByText(/Sin Calificación/i)).toBeInTheDocument()
   })
 });
