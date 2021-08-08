fb2458912b95303e32c735166b06f2ff99fb3f7d
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-08-08 03:03:37 -0500

    test: Más pruebas de vistas, 60% de cobertura alcanzada

--- src/pages/serviceDetails/serviceDetails.test.js
+++ src/pages/serviceDetails/serviceDetails.test.js
@@ -1,11 +1,40 @@
 /*Importamos las librerias principales*/
 import React from "react";
+import { shallow } from 'enzyme';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node';
+import { render, cleanup } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router, Route } from 'react-router-dom';
+import GlobalEnv from '../../GlobalEnv';
 import ServiceDetails from "./index";
-import { shallow } from "enzyme";
 /*Renderizado de la vista Service Details*/
 describe("<ServiceDetails></ServiceDetails>", () => {
+  const server = setupServer(
+    rest.post(`${GlobalEnv.host}/service-auth`, (req, res, ctx) => {
+      return res(ctx.json({ greeting: 'hello there' }))
+    }),
+  )
+
+  beforeAll(() => { server.listen()})
+  afterEach(() => { server.resetHandlers() })
+  afterAll(() => { server.close() })
+
   it('render `ServiceDetails`', () => {
     const wrapper = shallow(<ServiceDetails />);
     expect(wrapper).toMatchSnapshot()
   });
+
+  it('show Cargando', () => {
+    const history = createMemoryHistory();
+    server.use(
+      rest.post(`${GlobalEnv.host}/service-auth`, (req, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    )
+    render(<Router history={history}>
+      <ServiceDetails /> 
+  </Router>);
+  expect(screen.getByRole('service-details')).toHaveTextContent('Cargando...')
+  })
 });
