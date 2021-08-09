51e8e6c4a2cbc6ca57d4325feef00c6eaba94014
KoriAntunez <kori.antunez@unmsm.edu.pe>
2021-08-09 04:12:30 -0500

    useimput

--- src/hooks/useInput.test.js
+++ src/hooks/useInput.test.js
@@ -6,6 +6,9 @@ import UseInput from "./useInput";
 import { useInput } from "./useInput";
 import { render, fireEvent } from '@testing-library/react';
 import { act } from '@testing-library/react-hooks';
+import TestRenderer, { act } from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
 /*Renderizado del componente UseInput*/
 describe("UseInput", () => {
   it('render "UseInput"', () => {
@@ -18,15 +21,26 @@ describe("UseInput", () => {
     fireEvent.change(input, {target: {value: '23'}})
     expect(input.value).toBe('23')
   })
-it('should render handleDateRange function', () => {
-    const UseInput = jest.fn();
-    const render = shallow(<Assignment />);
-    
+  it('should handle onChange event', () => {
+    const testRenderer = TestRenderer.create(<useInput />);
+    const testInstance = testRenderer.root;
+    expect(testInstance.findByType('input').props.value).toEqual('');
+    const mEvent = { target: { value: 'teresa teng' } };
     act(() => {
-      render.find(SelectDropdown).first().simulate(UseInput, { target: { value: 'A' } });
+      testInstance.findByType('input').props.onChange(mEvent);
     });
-    expect(UseInput).toBeCalledWith('A');
-    expect(render).toMatchSnapshot();
-    render.unmount();
+    expect(testInstance.findByType('input').props.value).toEqual('teresa teng');
+  });
+
+  it('should handle onChange event when use shallow render', () => {
+    const shallowRenderer = ShallowRenderer.createRenderer();
+    shallowRenderer.render(<useInput/>);
+    let tree = shallowRenderer.getRenderOutput();
+    let input = tree.props.children[0];
+    const mEvent = { target: { value: 'teresa teng' } };
+    input.props.onChange(mEvent);
+    tree = shallowRenderer.getRenderOutput();
+    input = tree.props.children[0];
+    expect(input.props.value).toEqual('teresa teng');
   });
 });
