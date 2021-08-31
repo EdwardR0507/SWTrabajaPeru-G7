8fcfa50380c449ebef406259730a92a939be76c8
EdwardR0507 <ramosedward21@gmail.com>
2021-08-30 20:08:42 -0500

    final commit

--- src/pages/socialProfile/index.js
+++ src/pages/socialProfile/index.js
@@ -1,7 +1,6 @@
 /*Importamos las librerias principales*/
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router";
-import { useHistory } from "react-router";
+import { useLocation, useHistory } from "react-router";
 import { Container, Grid, Box } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import ProfileCard from "../../components/Cards/ProfileCard";
@@ -32,56 +31,53 @@ export default function SocialProfile() {
 
   //Información del usuario
   useEffect(() => {
-    console.log(state)
+    console.log(state);
     //Cambiar post por get cuando se arregle
-    if(!localStorage.hasOwnProperty("User_session")){
+    if (!localStorage.hasOwnProperty("User_session")) {
       history.push({
-        pathname: "/signup"
-      })
-    }
-    else{
-      token = localStorage.getItem("User_session")
-      token = token.slice(1, -1)
-      fetchData(token, "GET", "user-auth", "GET_MY_USER")
-      .then((res) => {
-        console.log(res);
-        setUser(res);
-        if (location.pathname === "/myAccount") {
-          setProfile(res)
-        }
-        else if (location.pathname === "/profile") {
-          fetchData(token, "POST", "user-auth", "OBTAIN_USER", { us_id: state?.idUser }).then(
-            (res) => {
-              console.log(res);
-              setProfile(res[0])
-            }
-          );
-        }
-      })
-      .catch((err) => {
-        console.log(err);
+        pathname: "/signup",
       });
+    } else {
+      token = localStorage.getItem("User_session");
+      token = token.slice(1, -1);
+      fetchData(token, "GET", "user-auth", "GET_MY_USER")
+        .then((res) => {
+          console.log(res);
+          setUser(res);
+          if (location.pathname === "/myAccount") {
+            setProfile(res);
+          } else if (location.pathname === "/profile") {
+            fetchData(token, "POST", "user-auth", "OBTAIN_USER", {
+              us_id: state?.idUser,
+            }).then((res) => {
+              console.log(res);
+              setProfile(res[0]);
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }, [state]);
 
   useEffect(() => {
-    token = localStorage.getItem("User_session")
-    token = token.slice(1, -1)
+    token = localStorage.getItem("User_session");
+    token = token.slice(1, -1);
     if (location.pathname === "/myAccount") {
-      fetchData(token, "GET", "service-auth", "GET_MY_SERVICES").then(
-        (res) => {
-          console.log(res);
-          setServices(res);
-        }
-      );
+      fetchData(token, "GET", "service-auth", "GET_MY_SERVICES").then((res) => {
+        console.log(res);
+        setServices(res);
+      });
     }
     //Falta agregar la obtención de servicios de otro usuario
     else if (location.pathname === "/profile") {
-      fetchData(token, "POST", "service-auth", "GET_OTHERS_SERVICES", { us_id: state?.idUser })
-        .then((res) => {
-          console.log(res);
-          setServices(res)
-        })
+      fetchData(token, "POST", "service-auth", "GET_OTHERS_SERVICES", {
+        us_id: state?.idUser,
+      }).then((res) => {
+        console.log(res);
+        setServices(res);
+      });
     }
   }, [state?.token]);
 
